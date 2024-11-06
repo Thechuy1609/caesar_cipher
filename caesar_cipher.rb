f842531b5df7dc1a67701a4be2850f6c3450479b
Thechuy1609 <blitzdashgd@gmail.com>
2024-11-06 18:16:52 -0500

    done now

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,16 @@
 
 def cipher(str,shift)
 string = str.bytes ## got ascci numbers
-cipher = string.map {|x| x + shift}
-ciphered = cipher.map {|x| x.chr}
-print ciphered.join.gsub("#", " ").gsub("$","!").gsub("B","?").gsub("%", " ").gsub("/", "W")
+cipher = string.map do |x| 
+  if x.between?(97,122)
+    ((x - 97 + shift) % 26 + 97).chr
+  elsif x.between?(65,90)
+    ((x - 65 + shift) % 26 + 65).chr
+  else
+    x.chr
+  end
+    end
+print cipher.join
 
 end
 cipher("What a string!",5)
